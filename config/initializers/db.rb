26b0aeb456edd832fe33995c6f5b086e32a6c429
K-2052 <k@2052.me>
2014-07-01 18:46:39 -0700

    fix typo in db initializer

--- config/initializers/db.rb
+++ config/initializers/db.rb
@@ -1,3 +1,3 @@
-uri = ENV['MONGOHQ_URL'] || ENV['MONGOLAB_URI'] || ENV['MONGODB_URI'] || ENV['MONGO_URL'] "mongodb://localhost:27017/Notes_#{Notes.env}"
+uri = ENV['MONGOHQ_URL'] || ENV['MONGOLAB_URI'] || ENV['MONGODB_URI'] || ENV['MONGO_URL'] || "mongodb://localhost:27017/Notes_#{Notes.env}"
 
 MongoMapper.setup({"#{Notes.env}" => {'uri' => uri}}, Notes.env)
